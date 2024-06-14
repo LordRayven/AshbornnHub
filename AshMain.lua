c06f565961d2e1a03f937c85d0f1a4cc2b0f39b1
Rayven Rubia <63869687+LordRayven@users.noreply.github.com>
2024-06-14 14:56:46 +0800

    Update AshMain.lua

--- AshMain.lua
+++ AshMain.lua
@@ -1,23 +1,3 @@
--- Ban list
-local banList = {
-    [6150337449] = true
-}
-
--- Function to check if a player is banned
-function isBanned(playerId)
-    return banList[playerId] ~= nil
-end
-
--- Get the local player
-local player = game.Players.LocalPlayer
-
--- Check if the local player is banned and kick if necessary
-if isBanned(player.UserId) then
-    player:Kick("You have been banned from this game. Please contact support for more information.")
-end
-
-
-
 repeat wait() until game:IsLoaded()
 
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
