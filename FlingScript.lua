fa3a8af658b9ee9a86ff37cfd56bc0597674eff1
Rayven Rubia <63869687+LordRayven@users.noreply.github.com>
2024-06-21 19:06:06 +0800

    Update FlingScript.lua

--- FlingScript.lua
+++ FlingScript.lua
@@ -215,12 +215,24 @@ end
 
 -- Whitelisted User IDs
 local WhitelistedUserIDs = {
-    [1414978355] = true,
-    [290931] = true,
-    [4072731377] = true,
-    [2911976621] = true,
-    [129215104] = true
+    ["129215104"] = true,
+    ["6069697086"] = true,
+    ["4072731377"] = true,
+    ["6150337449"] = true,
+    ["1571371222"] = true,
+    ["2911976621"] = true,
+    ["2729297689"] = true
+    ["6150320395"] = true,
+    ["301098121"] = true,
+    ["773902683"] = true,
+    ["290931"] = true,
+    ["671905963"] = true,
+    ["3129701628"] = true,
+    ["3063352401"] = true,
+    ["3129413184"] = true
+
 }
+
 
 for _,x in next, Targets do
     local TPlayer = GetPlayer(x)
