f22d4f1658d424c2a7f649214f0ed5a16d35da40
Rayven Rubia <63869687+LordRayven@users.noreply.github.com>
2024-06-23 18:39:02 +0800

    Update ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -1,186 +1,167 @@
--- made by rang#2415
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
 
-local ownerUserIds = {
-    [290931] = true,
+local premiums = {
+    [6069697086] = true,
     [4072731377] = true,
+    [6150337449] = true,
+    [1571371222] = true,
     [2911976621] = true,
-    [129215104] = true
+    [2729297689] = true,
+    [6150320395] = true,
+    [301098121] = true,
+    [773902683] = true,
+    [671905963] = true,
+    [3129701628] = true,
+    [3063352401] = true,
+    [3129413184] = true
+}
+
+local monarchs = {
+    [129215104] = true,
+    [6135258891] = true,
+    [290931] = true
 }
 
 local Config = {
-    Box = false,
-    BoxOutline = false,
-    BoxColor = Color3.fromRGB(255, 255, 255),
-    BoxOutlineColor = Color3.fromRGB(0, 0, 0),
-    HealthBar = false,
-    HealthBarSide = "Left",
     Names = true,
     NamesOutline = true,
     NamesColor = Color3.fromRGB(255, 255, 255),
     NamesOutlineColor = Color3.fromRGB(0, 0, 0),
-    NamesFont = 3,  -- Increase font type for better readability
-    NamesSize = 16  -- Increase size for better visibility
+    NamesFont = 3,
+    NamesSize = 16
 }
 
 local roles = {}
+local lastUpdate = 0
 
 local function roleUpdater()
     while true do
-        roles = ReplicatedStorage:FindFirstChild("GetPlayerData", true):InvokeServer()
-        wait(1)  -- Update every second
+        if os.time() - lastUpdate > 2 then
+            local success, result = pcall(function()
+                return ReplicatedStorage:FindFirstChild("GetPlayerData", true):InvokeServer()
+            end)
+            if success then
+                roles = result
+                lastUpdate = os.time()
+            end
+        end
+        wait(2) -- Increased the wait time to reduce frequency
     end
 end
 
 spawn(roleUpdater)
 
 local function IsAlive(Player)
-    for i, v in pairs(roles) do
-        if Player.Name == i then
-            if not v.Killed and not v.Dead then
-                return true
-            else
-                return false
-            end
-        end
+    local playerData = roles[Player.Name]
+    if playerData then
+        return not (playerData.Killed or playerData.Dead)
     end
     return false
 end
 
 local function getRoleColor(player)
-    if ownerUserIds[player.UserId] then
-        return Color3.fromRGB(128, 0, 128) -- Purple color for specific UserIds
-    end
-
-    for i, v in pairs(roles) do
-        if v.Role == "Murderer" and i == player.Name then
+    local playerData = roles[player.Name]
+    if playerData then
+        if playerData.Role == "Murderer" then
             return Color3.fromRGB(225, 0, 0) -- Red color
-        elseif v.Role == "Sheriff" and i == player.Name then
+        elseif playerData.Role == "Sheriff" then
             return Color3.fromRGB(0, 0, 225) -- Blue color
-        elseif v.Role == "Hero" and i == player.Name then
+        elseif playerData.Role == "Hero" then
             return Color3.fromRGB(255, 255, 0) -- Yellow color
         end
     end
     return Color3.fromRGB(0, 225, 0) -- Green color for alive players
 end
 
+local function getTitleColor(player)
+    if premiums[player.UserId] then
+        return Color3.fromRGB(0, 0, 139) -- Dark blue color for premiums
+    elseif monarchs[player.UserId] then
+        return Color3.fromRGB(128, 0, 128) -- Purple color for monarchs
+    end
+    return Config.NamesColor -- Default color
+end
+
 local function CreateEsp(Player)
-    local Box = Drawing.new("Square")
-    local BoxOutline = Drawing.new("Square")
+    local Title = Drawing.new("Text")
     local Name = Drawing.new("Text")
-    local HealthBar = Drawing.new("Square")
-    local HealthBarOutline = Drawing.new("Square")
 
-    local Updater = game:GetService("RunService").RenderStepped:Connect(function()
+    local function UpdateEsp()
         if Player.Character and Player.Character:FindFirstChild("Humanoid") and Player.Character:FindFirstChild("HumanoidRootPart") and Player.Character.Humanoid.Health > 0 and Player.Character:FindFirstChild("Head") then
-            local HeadPos, IsVisible = workspace.CurrentCamera:WorldToViewportPoint(Player.Character.Head.Position + Vector3.new(0, 0.2, 0))
-            local scale = 1 / (HeadPos.Z * math.tan(math.rad(workspace.CurrentCamera.FieldOfView * 0.5)) * 2) * 100
-            local width, height = math.floor(40 * scale), math.floor(60 * scale)
-
-            if Config.Box then
-                Box.Visible = IsVisible
-                Box.Color = Config.BoxColor
-                Box.Size = Vector2.new(width, height)
-                Box.Position = Vector2.new(HeadPos.X - Box.Size.X / 2, HeadPos.Y - Box.Size.Y)
-                Box.Thickness = 1
-                Box.ZIndex = 69
-
-                if Config.BoxOutline then
-                    BoxOutline.Visible = IsVisible
-                    BoxOutline.Color = Config.BoxOutlineColor
-                    BoxOutline.Size = Box.Size
-                    BoxOutline.Position = Box.Position
-                    BoxOutline.Thickness = 3
-                    BoxOutline.ZIndex = 1
-                else
-                    BoxOutline.Visible = false
-                end
-            else
-                Box.Visible = false
-                BoxOutline.Visible = false
-            end
+            local HeadPos, IsVisible = workspace.CurrentCamera:WorldToViewportPoint(Player.Character.Head.Position + Vector3.new(0, 2, 0))
+            local height = 60
 
             if Config.Names then
+                local playerDistance = (workspace.CurrentCamera.CFrame.Position - Player.Character.HumanoidRootPart.Position).Magnitude
+
+                Title.Visible = IsVisible
+                Title.Center = true
+                Title.Outline = Config.NamesOutline
+                Title.OutlineColor = Config.NamesOutlineColor
+                Title.Font = Config.NamesFont
+                Title.Size = Config.NamesSize
+                Title.Color = getTitleColor(Player)
+
+                if premiums[Player.UserId] then
+                    Title.Text = "(Premium)"
+                elseif monarchs[Player.UserId] then
+                    Title.Text = "(Monarch)"
+                else
+                    Title.Text = ""
+                end
+
+                Title.Position = Vector2.new(HeadPos.X, HeadPos.Y - height * 0.5 - 20)
+
                 Name.Visible = IsVisible
                 if IsAlive(Player) then
                     Name.Color = getRoleColor(Player)
                 else
                     Name.Color = Color3.fromRGB(100, 100, 100) -- Gray color if not alive
                 end
-                Name.Text = Player.Name .. " " .. math.floor((workspace.CurrentCamera.CFrame.p - Player.Character.HumanoidRootPart.Position).magnitude) .. "m"
+                Name.Text = Player.Name .. " " .. string.format("%.1f", playerDistance) .. "m"
                 Name.Center = true
                 Name.Outline = Config.NamesOutline
                 Name.OutlineColor = Config.NamesOutlineColor
-                Name.Position = Vector2.new(HeadPos.X, HeadPos.Y - height * 0.5 - 10)
+                Name.Position = Vector2.new(HeadPos.X, HeadPos.Y - height * 0.5)
                 Name.Font = Config.NamesFont
                 Name.Size = Config.NamesSize
             else
+                Title.Visible = false
                 Name.Visible = false
             end
-
-            if Config.HealthBar then
-                HealthBarOutline.Visible = IsVisible
-                HealthBarOutline.Color = Color3.fromRGB(0, 0, 0)
-                HealthBarOutline.Filled = true
-                HealthBarOutline.ZIndex = 1
-
-                HealthBar.Visible = IsVisible
-                HealthBar.Color = Color3.fromRGB(255, 0, 0):lerp(Color3.fromRGB(0, 255, 0), Player.Character.Humanoid.Health / Player.Character.Humanoid.MaxHealth)
-                HealthBar.Thickness = 1
-                HealthBar.Filled = true
-                HealthBar.ZIndex = 69
-
-                local barPos = Vector2.new(HeadPos.X - Box.Size.X / 2, HeadPos.Y - Box.Size.Y)
-                if Config.HealthBarSide == "Left" then
-                    HealthBarOutline.Size = Vector2.new(2, height)
-                    HealthBarOutline.Position = barPos + Vector2.new(-3, 0)
-                    HealthBar.Size = Vector2.new(1, - (HealthBarOutline.Size.Y - 2) * (Player.Character.Humanoid.Health / Player.Character.Humanoid.MaxHealth))
-                    HealthBar.Position = HealthBarOutline.Position + Vector2.new(1, -1 + HealthBarOutline.Size.Y)
-                elseif Config.HealthBarSide == "Bottom" then
-                    HealthBarOutline.Size = Vector2.new(width, 3)
-                    HealthBarOutline.Position = barPos + Vector2.new(0, height + 2)
-                    HealthBar.Size = Vector2.new((HealthBarOutline.Size.X - 2) * (Player.Character.Humanoid.Health / Player.Character.Humanoid.MaxHealth), 1)
-                    HealthBar.Position = HealthBarOutline.Position + Vector2.new(1, -1 + HealthBarOutline.Size.Y)
-                elseif Config.HealthBarSide == "Right" then
-                    HealthBarOutline.Size = Vector2.new(2, height)
-                    HealthBarOutline.Position = barPos + Vector2.new(width + 1, 0)
-                    HealthBar.Size = Vector2.new(1, - (HealthBarOutline.Size.Y - 2) * (Player.Character.Humanoid.Health / Player.Character.Humanoid.MaxHealth))
-                    HealthBar.Position = HealthBarOutline.Position + Vector2.new(1, -1 + HealthBarOutline.Size.Y)
-                end
-            else
-                HealthBar.Visible = false
-                HealthBarOutline.Visible = false
-            end
         else
-            Box.Visible = false
-            BoxOutline.Visible = false
+            Title.Visible = false
             Name.Visible = false
-            HealthBar.Visible = false
-            HealthBarOutline.Visible = false
             if not Player then
-                Box:Remove()
-                BoxOutline:Remove()
+                Title:Remove()
                 Name:Remove()
-                HealthBar:Remove()
-                HealthBarOutline:Remove()
-                Updater:Disconnect()
+                return false
             end
+        end
+        return true
+    end
+
+    local Updater
+    Updater = RunService.RenderStepped:Connect(function()
+        if not UpdateEsp() then
+            Updater:Disconnect()
         end
     end)
 end
 
-for _, v in pairs(game:GetService("Players"):GetPlayers()) do
-    if v ~= game:GetService("Players").LocalPlayer then
+local function OnPlayerAdded(v)
+    if v ~= Players.LocalPlayer then
         CreateEsp(v)
         v.CharacterAdded:Connect(function() CreateEsp(v) end)
     end
 end
 
-game:GetService("Players").PlayerAdded:Connect(function(v)
-    if v ~= game:GetService("Players").LocalPlayer then
-        CreateEsp(v)
-        v.CharacterAdded:Connect(function() CreateEsp(v) end)
-    end
-end)
+for _, v in pairs(Players:GetPlayers()) do
+    OnPlayerAdded(v)
+end
+
+Players.PlayerAdded:Connect(OnPlayerAdded)
 
 return Config
