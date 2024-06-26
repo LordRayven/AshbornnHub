b95a2f50a753b8ce98c9f24002f49546bb229cd2
Rayven Rubia <63869687+LordRayven@users.noreply.github.com>
2024-06-26 23:04:22 +0800

    Update KeySol.lua

--- KeySol.lua
+++ KeySol.lua
@@ -1,9 +1,9 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
-local url = "https://link-target.net/480893/ashbornnhub-key-system-so"
+local url = "https://direct-link.net/480893/ashbornnhub-solara-key"
 
 local userkey
-local success, fetchedKey = pcall(game.HttpGet, game, "https://pastebin.com/raw/wGGGuyr8")
+local success, fetchedKey = pcall(game.HttpGet, game, "https://pastebin.com/raw/t72kpYK7")
 if success then
     userkey = fetchedKey
     Fluent:Notify({
